dd9a05d62ccb1eded5a47bd3d881f03778001603
Gregory Caesar <main@Gregorys-MacBook-Pro.local>
2023-07-10 13:36:43 -0400

    updated server.js location

--- db.js
+++ db.js
@@ -1,17 +1,9 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-const username = process.env.DATABASE_USER;
-const host = process.env.DB_HOST;
-const databaseName = process.env.DB_NAME;
-const password = process.env.DATABASE_PASS;
-
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'lifetracker',
-    password: "472237",
-    port: 5432,
+    connectionString: process.env.PRODUCTION_API_BASE_URL
 })
+
 
 module.exports = pool
